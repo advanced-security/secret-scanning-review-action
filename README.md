0cdf19683c54163d3c5359e14e49beb4a0507320
Chad Bentz <1760475+felickz@users.noreply.github.com>
2022-08-17 01:39:32 -0400

    V0.0.6 - Add Docs - Mermaid Architecture Readme - API comments - Support FailOnAlert via env var SSR_FAIL_ON_ALERT - Write-ActionDebug instead of Write-Verbose

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: 'Dependency Review'
-        uses: felickz/secret-scanning-review-action@v0.0.5-alpha
+        uses: felickz/secret-scanning-review-action@v0.0.6-alpha
         env:
             GITHUB_TOKEN: ${{ secrets.SECRET_SCAN_REVIEW_GITHUB_TOKEN }}
 ```
@@ -28,14 +28,52 @@ jobs:
 # Architecture
 * PowerShell based script wrapped in a GitHub NodeJS action
 
+```mermaid
+sequenceDiagram
+    autonumber
+    participant Repo as Repository
+    participant PR as Pull Request
+    participant Action as Action Workflow
+    participant API_PR as pulls<br/><br/>REST API
+    participant API_SECRET as secret-scanning<br/><br/> REST API
+
+    Repo->>PR: Create/Update PR
+    PR->>Action: invoke `pull_request` workflow
+    Action->>API_PR: GET PR
+    Action->>API_PR: GET PR Commits
+    loop Commits
+        Action->>Action: Build PR Commit SHA list      
+    end
+    Action->>API_SECRET: GET Secret Scanning Alerts
+    loop Secret Scanning Alerts
+        Action->>API_SECRET: GET Secret Scanning Alert List Locations
+        loop Secret Scanning Alert Locations
+        Action->>Action:Build List of Alert Initial Location SHAs that are<br/>contained in the PR SHA List (Step 5)
+        end
+    end
+
+    loop List of matching PR/Alerts
+      loop List of Locations for matching PR/Alerts       
+        Action->>PR:Writes an Action Warning to the message log<br/>creates an annotation associated with the file and line/col number.
+      end               
+    end       
+    Note right of PR: Annotations are visible<br/>on the PR Files changed rich diff
+```
+
 ## Secrets the action uses
 * [GITHUB_TOKEN](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token) with repo scope or security_events scope. For public repositories, you may instead use the public_repo scope.
 
-   * Unfortunately we cannot currently utilize the built in Actions `GITHUB_TOKEN` due to missing permissions on the `secret-scanning` api.  Therefore you must generate a PAT with these permissions, add the token as as cret in your repository, and assign the secret to the env variable in the workflow. See [Granting additional permissions](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#granting-additional-permissions)
+   * Unfortunately we cannot currently utilize the built in Actions `GITHUB_TOKEN` due to ommitted permissions on the `secret-scanning` api.  Therefore you must generate a PAT with these permissions, add the token as a secret in your repository, and assign the secret to the env variable in the workflow. See [Granting additional permissions](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#granting-additional-permissions)
+   * It is worth noting this PAT will have `sensitive` data access to return a list of plain text secrets that have been detected in your organization/repository.  At this point, a detected secret also implies any read repository access would provide the same level of access to the leaked secret.
 
 ## Environment variables the action uses
-* GITHUB_REPOSITORY = The owner / repository name.
-* GITHUB_REF - PR merge branch refs/pull/:prNumber/merge
+* Required
+  * GITHUB_TOKEN - token used to invoke REST APIs
+* Implicit
+  * GITHUB_REPOSITORY - The owner / repository name.
+  * GITHUB_REF - PR merge branch refs/pull/:prNumber/merge
+* Optional
+  * SSR_FAIL_ON_ALERT - overrides the FailOnAlert input 
 
 ## Dependencies
 * GitHub Dependencies
@@ -46,6 +84,7 @@ jobs:
 * Powershell Dependencies
     * `PowerShellForGitHub` ([Gallery](https://www.powershellgallery.com/packages/PowerShellForGitHub/0.16.1) / [Repo](https://github.com/Microsoft/PowerShellForGitHub)) - PowerShell wrapper for GitHub API
         * by [@microsoft](https://github.com/microsoft)
+        * NOTE: [Telemetry is collected via Application Insights](https://github.com/microsoft/PowerShellForGitHub/blob/master/USAGE.md#telemetry)
     * `GitHubActions` ([Gallery](https://www.powershellgallery.com/packages/GitHubActions/1.0.0.3) / [Repo](https://github.com/ebekker/pwsh-github-action-tools)) - PowerShell wrapper of the Github `@actions/core` [toolkit](https://github.com/actions/toolkit/tree/master/packages/core)
         * by [@ebekker](https://github.com/ebekker/)
 
--- action.ps1
+++ action.ps1
@@ -23,21 +23,23 @@ A simple example execution of the internal pwsh script against an Owner/Repo and
 
 .NOTES
 Features
-- Actions compatible
-  - GitHubActions module ( https://github.com/ebekker/pwsh-github-action-base)
-     - options: https://github.com/ebekker/pwsh-github-action-tools/blob/master/docs/GitHubActions/README.md
-  - Annotations - https://github.com/actions/toolkit/tree/main/packages/core#annotations
-     - warning message - https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-warning-message
-TODO LIST 
+    - Actions compatible
+        - GitHubActions module ( https://github.com/ebekker/pwsh-github-action-base)
+        - options: https://github.com/ebekker/pwsh-github-action-tools/blob/master/docs/GitHubActions/README.md
+    - PR File Annotations - https://github.com/actions/toolkit/tree/main/packages/core#annotations
+        - warning message - https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-warning-message
+
+ToDo List 
    - filter out alerts that are before the first commit of the PR
-   - graphQL instead of iterating over rest api / or parallel api calls
-   - support step debug logs == verbose mode (pwsh-github-action-tools support this?)
-   - options for FailOnAlert workflow switch
-      - if offending alert is in bypassed state, then it is not a failure
-      - whitelist of secret types (https://docs.github.com/en/enterprise-cloud@latest/code-security/secret-scanning/secret-scanning-patterns#supported-secrets-for-advanced-security)
-   -  GITHUB_TOKEN as param vs env var ... ex's : https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token
-   - Docs
-      - architecture mermaid chart
+   - graphQL instead of iterating over rest api / or parallel api calls   
+   - param vs env var (js script enhancement) ... ex's : https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token
+
+Future Enhancements
+    - add summary comment to PR if alert is found (1 comment per PR even if multiple runs?)
+    - options for FailOnAlert workflow switch
+        - if offending alert is in bypassed state, then it is not a failure
+        - whitelist of secret types (https://docs.github.com/en/enterprise-cloud@latest/code-security/secret-scanning/secret-scanning-patterns#supported-secrets-for-advanced-security)
+
 
 .LINK
 https://github.com/felickz/secret-scanning-review-action
@@ -53,25 +55,27 @@ Set-PSRepository PSGallery -InstallationPolicy Trusted
 #check if PowerShellForGitHub module is installed
 if (Get-Module -ListAvailable -Name PowerShellForGitHub -ErrorAction SilentlyContinue)
 {
-    Write-Verbose "PowerShellForGitHub module is installed"
+    Write-ActionDebug "PowerShellForGitHub module is installed"
 }
 else
 {
-    Write-Host "PowerShellForGitHub module is not installed.  Installing..."
+    Write-ActionInfo "PowerShellForGitHub module is not installed.  Installing from Gallery..."
     Install-Module -Name PowerShellForGitHub
+
+    #Disable Telemetry since we are accessing sensitive apis - https://github.com/microsoft/PowerShellForGitHub/blob/master/USAGE.md#telemetry
+    Set-GitHubConfiguration -DisableTelemetry -SessionOnly
 }
 
 #check if GitHubActions module is installed
 if (Get-Module -ListAvailable -Name GitHubActions -ErrorAction SilentlyContinue)
 {
-    Write-Verbose "GitHubActions module is installed"
+    Write-ActionDebug "GitHubActions module is installed"
 }
 else
 {
-    Write-Host "GitHubActions module is not installed.  Installing..."
+    Write-ActionInfo "GitHubActions module is not installed.  Installing from Gallery..."
     Install-Module -Name GitHubActions
 }
-
 
 #check if GITHUB_TOKEN is set
 if ($null -eq $env:GITHUB_TOKEN)
@@ -80,7 +84,7 @@ if ($null -eq $env:GITHUB_TOKEN)
 }
 else
 {
-    Write-Verbose "GITHUB_TOKEN is set"
+    Write-ActionDebug "GITHUB_TOKEN is set"
 }
 
 #configure github module with authentication token ... sample code taken from example 2 for GitHub Action!
@@ -114,22 +118,29 @@ else
 #Default Org / Repo for all GH api calls
 Set-GitHubConfiguration -DefaultOwnerName $OrganizationName -DefaultRepositoryName $RepositoryName
 
-#get PR info - https://docs.github.com/en/enterprise-cloud@latest/rest/pulls/pulls#get-a-pull-request
+<# API: GET PR  
+    - docs: https://docs.github.com/en/enterprise-cloud@latest/rest/pulls/pulls#get-a-pull-request
+    - format: /repos/{owner}/{repo}/pulls/{pull_number}
+#>
 try {
     $pr = Get-GitHubPullRequest -PullRequest $PullRequestNumber
 } catch {
     Set-ActionFailed -Message "Error getting '$OrganizationName/$RepositoryName' PR#$PullRequestNumber info.  Ensure GITHUB_TOKEN has proper repo permissions. (StatusCode:$($_.Exception.Response.StatusCode.Value__) Message:$($_.Exception.Message)"
 }
-Write-Host "PR#$PullRequestNumber '$($pr.Title)' has $($pr.commits) commits"
+Write-ActionInfo "PR#$PullRequestNumber '$($pr.Title)' has $($pr.commits) commits"
 ####@{url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/pulls/119; id=1022143435; node_id=PR_kwDOD0gIsM487KvL; html_url=https://github.com/octodemo/demo-vulnerabilities-ghas/pull/119; diff_url=https://github.com/octodemo/demo-vulnerabilities-ghas/pull/119.diff; patch_url=https://github.com/octodemo/demo-vulnerabilities-ghas/pull/119.patch; issue_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/issues/119; number=119; state=open; locked=False; title=test adding GCP API key; user=; body=; created_at=08/10/2022 02:23:05; updated_at=08/10/2022 19:49:58; closed_at=; merged_at=; merge_commit_sha=02b4b03dee89cb4d65112ab8f7f32756e4a1f684; assignee=; assignees=System.Object[]; requested_reviewers=System.Object[]; requested_teams=System.Object[]; labels=System.Object[]; milestone=; draft=False; commits_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/pulls/119/commits; review_comments_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/pulls/119/comments; review_comment_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/pulls/comments{/number}; comments_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/issues/119/comments; statuses_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/statuses/d5a2299dd7307a79ca6b8b3fbf5cf192e62a683d; head=; base=; _links=; author_association=COLLABORATOR; auto_merge=; active_lock_reason=; merged=False; mergeable=True; rebaseable=True; mergeable_state=blocked; merged_by=; comments=0; review_comments=0; maintainer_can_modify=True; commits=2; additions=3; deletions=0; changed_files=1}
 
-#commits_url https://docs.github.com/en/enterprise-cloud@latest/rest/pulls/pulls#list-commits-on-a-pull-request
+<# API: GET PR Commits  
+    - docs: https://docs.github.com/en/enterprise-cloud@latest/rest/pulls/pulls#list-commits-on-a-pull-request
+    - format: /repos/{owner}/{repo}/pulls/{pull_number}/commits
+#>
 $prCommitsUrl = [uri]$pr.commits_url
 try {
-$commits = Invoke-GHRestMethod -Method GET -Uri $prCommitsUrl.AbsolutePath
+    $commits = Invoke-GHRestMethod -Method GET -Uri $prCommitsUrl.AbsolutePath
 } catch {
     Set-ActionFailed -Message "Error getting '$OrganizationName/$RepositoryName' PR#$PullRequestNumber commits.  Ensure GITHUB_TOKEN has proper repo permissions. (StatusCode:$($_.Exception.Response.StatusCode.Value__) Message:$($_.Exception.Message)"
 }
+
 #for each PR commit add the commit sha to the list
 $prCommitShaList = @()
 foreach ($commit in $commits)
@@ -139,12 +150,17 @@ foreach ($commit in $commits)
     $prCommitShaList += $commit.sha
 }
 
-Write-Host "PR#$PullRequestNumber Commit SHA list: $($prCommitShaList -join ",")"
+Write-ActionInfo "PR#$PullRequestNumber Commit SHA list: $($prCommitShaList -join ",")"
 
 ####@{id=256379056; node_id=MDEwOlJlcG9zaXRvcnkyNTYzNzkwNTY=; name=demo-vulnerabilities-ghas; full_name=octodemo/demo-vulnerabilities-ghas; private=True; owner=; html_url=https://github.com/octodemo/demo-vulnerabilities-ghas; description=This repo contains examples of all security feature available for GitHub Enterprise and GHAS. Use it for demo purposes only.; fork=False; url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas; forks_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/forks; keys_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/keys{/key_id}; collaborators_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/collaborators{/collaborator}; teams_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/teams; hooks_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/hooks; issue_events_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/issues/events{/number}; events_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/events; assignees_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/assignees{/user}; branches_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/branches{/branch}; tags_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/tags; blobs_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/blobs{/sha}; git_tags_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/tags{/sha}; git_refs_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/refs{/sha}; trees_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/trees{/sha}; statuses_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/statuses/{sha}; languages_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/languages; stargazers_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/stargazers; contributors_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/contributors; subscribers_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/subscribers; subscription_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/subscription; commits_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/commits{/sha}; git_commits_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/commits{/sha}; comments_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/comments{/number}; issue_comment_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/issues/comments{/number}; contents_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/contents/{+path}; compare_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/compare/{base}...{head}; merges_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/merges; archive_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/{archive_format}{/ref}; downloads_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/downloads; issues_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/issues{/number}; pulls_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/pulls{/number}; milestones_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/milestones{/number}; notifications_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/notifications{?since,all,participating}; labels_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/labels{/name}; releases_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/releases{/id}; deployments_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/deployments; created_at=04/17/2020 02:17:12; updated_at=05/17/2022 21:03:00; pushed_at=08/10/2022 19:47:32; git_url=git://github.com/octodemo/demo-vulnerabilities-ghas.git; ssh_url=org-38940897@github.com:octodemo/demo-vulnerabilities-ghas.git; clone_url=https://github.com/octodemo/demo-vulnerabilities-ghas.git; svn_url=https://github.com/octodemo/demo-vulnerabilities-ghas; homepage=; size=24244; stargazers_count=8; watchers_count=8; language=JavaScript; has_issues=True; has_projects=True; has_downloads=True; has_wiki=True; has_pages=False; forks_count=8; mirror_url=; archived=False; disabled=False; open_issues_count=71; license=; allow_forking=True; is_template=False; web_commit_signoff_required=False; topics=System.Object[]; visibility=internal; forks=8; open_issues=71; watchers=8; default_branch=main; permissions=; temp_clone_token=AANNZW2QMTIDF37VXPAAASLC6YJFM; allow_squash_merge=True; allow_merge_commit=True; allow_rebase_merge=True; allow_auto_merge=False; delete_branch_on_merge=False; allow_update_branch=False; use_squash_pr_title_as_default=False; squash_merge_commit_message=COMMIT_MESSAGES; squash_merge_commit_title=COMMIT_OR_PR_TITLE; merge_commit_message=PR_TITLE; merge_commit_title=MERGE_MESSAGE; organization=; security_and_analysis=; network_count=8; subscribers_count=0}
 ####$repo = Get-GitHubRepository -OwnerName $OrganizationName -RepositoryName $RepositoryName
 
-#Query ALL(open/resolved) secret scanning alerts for the repository - https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-secret-scanning-alerts-for-a-repository
+<# API: GET Secret Scanning Alerts
+    - docs: https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-secret-scanning-alerts-for-a-repository
+    - format: /repos/{owner}/{repo}/secret-scanning/alerts
+    - note: This endpoint is only available for organizations and repositories in the Enterprise Cloud. 
+    - note: This endpoint returns ALL (both: open and resolved) secret scanning alerts.
+#>
 $repoAlertsUrl = "/repos/$OrganizationName/$RepositoryName/secret-scanning/alerts"
 try {
 $alerts = Invoke-GHRestMethod -Method GET -Uri $repoAlertsUrl
@@ -156,11 +172,13 @@ $alerts = Invoke-GHRestMethod -Method GET -Uri $repoAlertsUrl
 $alertCount = 0
 $alertsInitiatedFromPr = @()
 foreach ($alert in $alerts) {
+    <# API: GET Secret Scanning Alert List Locations 
+    - docs: https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-locations-for-a-secret-scanning-alert
+    - format: /repos/{owner}/{repo}/secret-scanning/alerts/{alert_number}/locations
+    - returns: 1 "location" object per file where secret detected
+    - note: details.commit_sha - SHA of the commit where the secret was detected
+    #>
     $repoAlertLocationUrl = [uri]$alert.locations_url
-
-    #Secret Scanning List Locations API - https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-locations-for-a-secret-scanning-alert
-    # - RESPONSE: 1 "location" object per file where secret detected
-    #    - details.commit_sha - SHA of the commit where the secret was detected
     try {
         $locations = Invoke-GHRestMethod -Method GET -Uri $repoAlertLocationUrl.AbsolutePath
     } catch {
@@ -172,13 +190,13 @@ foreach ($alert in $alerts) {
         #@{path=secrets.yml; start_line=1; end_line=1; start_column=11; end_column=32; blob_sha=d233fb964b86e09d4a99bba85c6006dcc4c9258f; blob_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/blobs/d233fb964b86e09d4a99bba85c6006dcc4c9258f; commit_sha=44d3503204f61baffcad1f2293ab9dd41db4820a; commit_url=https://api.github.com/repos/octodemo/demo-vulnerabilities-ghas/git/commits/44d3503204f61baffcad1f2293ab9dd41db4820a}
         $alertInitialCommitSha = $location.details.commit_sha
 
-        #if alertInitialCommitSha in list of commit shas, then add alert to list of alerts to delete
+        #if alertInitialCommitSha in list of commit shas, then add location to list to further add to the alert list
         if ($alertInitialCommitSha -in $prCommitShaList) {
-            Write-Verbose "YES! Found a secret scanning alert (# $($alert.number)) initial commit sha: $alertInitialCommitSha that originated from a PR#$PullRequestNumber commit"
+            Write-ActionDebug "YES! Found a secret scanning alert (# $($alert.number)) on initial commit sha: $alertInitialCommitSha that originated from a PR#$PullRequestNumber commit"
             $locationMatches += $location
         }
         else {
-            Write-Verbose "NO! Did not find a secret scanning alert (# $($alert.number)) initial commit sha: $alertInitialCommitSha that originated from a PR#$PullRequestNumber commit"
+            Write-ActionDebug "NO! Did not find a secret scanning alert (# $($alert.number)) on initial commit sha: $alertInitialCommitSha that originated from a PR#$PullRequestNumber commit"
         }
     }
 
@@ -205,16 +223,44 @@ foreach ($alert in $alertsInitiatedFromPr) {
     foreach($location in $alert.locations) {        
         # TODO - no support for ?Title? .. send PR to maintainer!
         $numSecretsAlertLocationsDetected++
+        # Writes an Action Warning to the message log and creates an annotation associated with the file and line/col number.
+        #   -docs: https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message
         Write-ActionWarning -Message "$($alert.push_protection_bypassed?'Bypassed':'New') Secret Detected in Pull Request #$PullRequestNumber Commit SHA:$($location.details.commit_sha.SubString(0,7)).  Secret:$($alert.html_url) Commit:$($pr.html_url)/commits/$($location.details.commit_sha)" -File $location.details.path -Line $location.details.start_line -Col $location.details.start_column
     }  
 }
 
-Write-Host "SECRET SCANNING REVIEW SUMMARY: Found [$numSecretsAlertsDetected] alert$($numSecretsAlertsDetected -eq 1 ? '' : 's') across [$numSecretsAlertLocationsDetected] location$($numSecretsAlertLocationsDetected -eq 1 ? '' : 's') that originated from a PR#$PullRequestNumber commit"
+#TODO - consider outputing this summary to a comment on the PR
+# #Add 1 time comment to PR with summary of alerts found
+# $comment = @{
+#     body = "Found $numSecretsAlertsDetected secret scanning alerts in this Pull Request.  $numSecretsAlertLocationsDetected of those alerts were detected in this Pull Request."
+# }
+# $commentUrl = "/repos/$OrganizationName/$RepositoryName/pulls/$PullRequestNumber/comments"
+# try {
+#     $comment = Invoke-GHRestMethod -Method POST -Uri $commentUrl -Body $comment
+# } catch {
+#     Set-ActionFailed -Message "Error adding comment to '$OrganizationName/$RepositoryName' Pull Request#$PullRequestNumber.  Ensure GITHUB_TOKEN has proper repo permissions. (StatusCode:$($_.Exception.Response.StatusCode.Value__) Message:$($_.Exception.Message)"
+# }
+
+$summary = "SECRET SCANNING REVIEW SUMMARY: Found [$numSecretsAlertsDetected] alert$($numSecretsAlertsDetected -eq 1 ? '' : 's') across [$numSecretsAlertLocationsDetected] location$($numSecretsAlertLocationsDetected -eq 1 ? '' : 's') that originated from a PR#$PullRequestNumber commit"
+
 
 #if any alerts were found in FailOnAlert mode, exit with error code 1
+# workaround - read $FailOnAlert from the environment variable
+Write-ActionDebug "FailOnAlert is set to '$FailOnAlert'. $($null -ne $env:SSR_FAIL_ON_ALERT ? "Overridden by environment variable SSR_FAIL_ON_ALERT: '$env:SSR_FAIL_ON_ALERT'" : $null)" 
+if($null -ne $env:SSR_FAIL_ON_ALERT)
+{
+    try {
+        $FailOnAlert = [System.Convert]::ToBoolean($env:SSR_FAIL_ON_ALERT) 
+     } catch [FormatException] {
+        $FailOnAlert = $false
+    }
+}
+
+
 if($alertsInitiatedFromPr.Count -gt 0 -and $FailOnAlert) {
-    Set-ActionFailed -Message "Found $($alertsInitiatedFromPr.count) secret scanning alert(s) that originated from a PR#$PullRequestNumber commit"
+    Set-ActionFailed -Message $summary
 }
 else {
+    Write-ActionInfo $summary    
     exit 0
 }
