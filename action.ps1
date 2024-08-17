cf86a579582f6d602d94fce4d85a76cb4d0ffad3
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-08-16 23:17:03 -0400

    Better errors

--- action.ps1
+++ action.ps1
@@ -328,7 +328,7 @@ if (!$DisablePRComment -and $alertsInitiatedFromPr.Count -gt 0) {
     }
 }
 else {
-    Write-ActionDebug "DisablePRComment is set to $DisablePRComment and alertsInitiatedFromPr is $($alertsInitiatedFromPr.Count) , skipping PR comment update"
+    Write-ActionDebug "Skipping PR comment update - DisablePRComment is set to $DisablePRComment and alertsInitiatedFromPr is $($alertsInitiatedFromPr.Count)"
 }
 
 #Output Step Summary - To the GITHUB_STEP_SUMMARY environment file. GITHUB_STEP_SUMMARY is unique for each step in a job
